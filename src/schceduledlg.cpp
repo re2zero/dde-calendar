a28e4f9faa2411fe38c2e502d54532a51395c865
jack <jack@deepin.io>
2020-01-07 12:14:36 +0800

    fix:Modify schedule prompt box title
    Modify schedule prompt box title

--- src/schceduledlg.cpp
+++ src/schceduledlg.cpp
@@ -159,7 +159,7 @@ void CSchceduleDlg::slotOkBt()
         return;
     }
     if (beginDateTime > endDateTime) {
-        QMessageBox::information(this, (""), tr("End time must be greater than start time"));
+        QMessageBox::information(this, ("    "), tr("End time must be greater than start time"));
         return;
     }
     if (m_type == 1) scheduleDtailInfo.id = 0;
