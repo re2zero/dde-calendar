69b255c5b57b080ae32a415ea933a1b51568bdf5
zhengxiaokang <zhengxiaokang@uniontech.com>
2020-05-25 11:44:32 +0800

    Type:27103 Description:My calendar popup scroll bar is blocked RootCause:Null Solution:fix code

--- src/myschceduleview.cpp
+++ src/myschceduleview.cpp
@@ -438,7 +438,7 @@ void CMySchceduleView::initUI()
 
     area = new QScrollArea (this);
     area->setFrameShape(QFrame::NoFrame);
-    area->setFixedWidth(386);
+    area->setFixedWidth(376);
     DPalette pa = area->palette();
     if (themetype == 0 || themetype == 1) {
         pa.setColor(DPalette::WindowText, QColor("#2C4767"));
