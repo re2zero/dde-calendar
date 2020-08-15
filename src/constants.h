017370fa7c37aa8d1bcbea6768ac8efee095d117
zhengxiaokang <zhengxiaokang@uniontech.com>
2020-08-15 10:33:05 +0800

    chore: 增加QueryEarliestYear常量
    增加QueryEarliestYear常量
    
    Log: 增加QueryEarliestYear常量
    
    link:

--- src/constants.h
+++ src/constants.h
@@ -29,6 +29,8 @@ static const int CellHeight = 70;
 
 static const int CellHighlightWidth = 80;
 static const int CellHighlightHeight = 70;
+
+static const int QueryEarliestYear = 1900;
 }
 namespace DDEYearCalendar {
 static const int YHeaderItemWidth = 20;
