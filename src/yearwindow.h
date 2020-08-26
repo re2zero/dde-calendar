b9ef6ecf6ca6d949126011e40b91ad1f19ca0532
zhengxiaokang <zhengxiaokang@uniontech.com>
2020-08-26 17:26:03 +0800

    chore: 为yearwindow.h文件中的函数添加注释
    为yearwindow.h文件中的函数添加注释
    
    Log: 为yearwindow.h文件中的函数添加注释
    
    link:

--- src/yearwindow.h
+++ src/yearwindow.h
@@ -47,40 +47,157 @@ class CYearWindow: public QMainWindow
 public:
     CYearWindow(QWidget *parent = nullptr);
     ~CYearWindow() override;
+    /**
+     * @brief setDate 设置年视图当前显示的时间
+     * @param date 年视图当前显示的时间
+     */
     void setDate(QDate date);
+    /**
+     * @brief initUI 初始化年视图的界面显示
+     */
     void initUI();
+    /**
+     * @brief initConnection 初始化信号和槽的连接
+     */
     void initConnection();
+    /**
+     * @brief setLunarVisible 设置年视图是否显示阴历信息
+     * @param state 是否显示阴历信息
+     */
     void setLunarVisible(bool state);
+    /**
+     * @brief setTheMe 设置系统主题
+     * @param type 主题类型
+     */
     void setTheMe(int type = 0);
+    /**
+     * @brief setSearchWFlag 设置是否在进行搜索
+     * @param flag 是否所搜的标志
+     */
     void setSearchWFlag(bool flag);
+    /**
+     * @brief clearSearch
+     */
     void clearSearch();
+    /**
+     * @brief setSearchText
+     * @param str
+     */
     void setSearchText(QString str);
+    /**
+     * @brief getScheduleInfo 判断一年中每一天是否有日程信息
+     */
     void getScheduleInfo();
 signals:
+    /**
+     * @brief dateSelected
+     * @param date
+     * @param detail
+     */
     void dateSelected(const QDate date, const CaLunarDayInfo &detail) const;
+    /**
+     * @brief signalsReturnTodayUpdate 点击返回今天按钮发出信号
+     * @param w 年视图窗口
+     */
     void signalsReturnTodayUpdate(QMainWindow *w);
+    /**
+     * @brief signalsWUpdateShcedule
+     * @param w
+     * @param id
+     */
     void signalsWUpdateShcedule(QMainWindow *w, int id = 0);
+    /**
+     * @brief signaldoubleclickDate 双击鼠标发出信号
+     * @param date 双击的日期
+     */
     void signaldoubleclickDate(QDate date);
+    /**
+     * @brief signalselectMonth 鼠标选择的日期信号
+     * @param date 选择的日期
+     */
     void signalselectMonth(QDate date);
+    /**
+     * @brief signalselectWeekwindow 选择周视图的信号
+     * @param date 选择的日期
+     */
     void signalselectWeekwindow(QDate date);
+    /**
+     * @brief signalCurrentDate 设置当前时间的信号
+     * @param date 当前时间
+     */
     void signalCurrentDate(QDate date);
+    /**
+     * @brief signalupdateschcedule 更新日程的信号
+     */
     void signalupdateschcedule();
 private slots:
+    /**
+     * @brief slotprev 通过鼠标点击左箭头切换到上一年，刷新年视图下的所有内容，并隐藏日程展示浮窗。
+     */
     void slotprev();
+    /**
+     * @brief slotnext 通过鼠标点击右箭头切换到下一年，并刷新年视图下的所有内容，并隐藏日程展示浮窗。
+     */
     void slotnext();
+    /**
+     * @brief slottoday 通过鼠标点击返回今天按钮返回到今天，并刷新年视图下的所有内容，并隐藏日程展示浮窗。
+     */
     void slottoday();
+    /**
+     * @brief slotsearchDateSelect 设置选择的时间
+     * @param date 时间
+     */
     void slotsearchDateSelect(QDate date);
+    /**
+     * @brief slotTransitSearchSchedule
+     * @param id
+     */
     void slotTransitSearchSchedule(int id = 0);
 public slots:
+    /**
+     * @brief slotSetSchceduleHide 隐藏日程浮框
+     */
     void slotSetSchceduleHide();
+    /**
+     * @brief slotReturnTodayUpdate
+     */
     void slotReturnTodayUpdate();
+    /**
+     * @brief slotupdateSchedule 更新当前时间
+     * @param id
+     */
     void slotupdateSchedule(const int id);
+    /**
+     * @brief setYearData 设置年视图阴历和按钮信息
+     */
     void setYearData();
+    /**
+     * @brief slotUpdateCurrentDate 设置年视图当前时间的信息
+     * @param date 时间
+     */
     void slotUpdateCurrentDate(const QDate &date);
 protected:
+    /**
+     * @brief wheelEvent 通过鼠标中间的滚轮滚动切换年份，并刷新年视图下的所有内容。
+     * @param event 鼠标滚轮事件
+     */
     void wheelEvent(QWheelEvent *event) override;
+    /**
+     * @brief eventFilter 过滤器，过滤返回今天的按钮事件
+     * @param watched 事件对象
+     * @param event 时间类型
+     * @return false
+     */
     bool eventFilter(QObject *watched, QEvent *event) override;
+    /**
+     * @brief mousePressEvent 鼠标单击事件，单击日期区域外，隐藏日程浮框
+     * @param event 鼠标事件
+     */
     void mousePressEvent(QMouseEvent *event) override;
+    /**
+     * @brief resizeEvent 窗口大小调整事件，搜索时，调整边框大小
+     * @param event 窗口大小调整事件
+     */
     void resizeEvent(QResizeEvent *event) override;
 private:
     DFrame *m_contentBackground = nullptr;
@@ -106,34 +223,108 @@ class YearFrame : public DFrame
 {
     Q_OBJECT
 public:
+    /**
+     * @brief YearFrame 构造函数
+     * @param parent 父类
+     */
     explicit YearFrame(DWidget *parent = nullptr);
+    /**
+      *@brief ~YearFrame 析构函数
+      */
     ~YearFrame() override;
+    /**
+     * @brief setDate 设置当前时间
+     * @param date 时间
+     */
     void setDate(QDate &date);
+    /**
+     * @brief getInfoAndSetLineFlag 获取日程信息，设置是否有日程的标志
+     */
     void getInfoAndSetLineFlag();
+    /**
+     * @brief setTheMe 根据系统主题类型设置颜色
+     * @param type 系统主题类型
+     */
     void setTheMe(int type = 0);
+    /**
+     * @brief setSearchWFlag 设置搜索的标志
+     * @param flag 是否进行了搜索
+     */
     void setSearchWFlag(bool flag);
+    /**
+     * @brief getLunarYear 获取阴历年信息
+     * @return 阴历年信息字符串
+     */
     QString getLunarYear()const
     {
         return m_LunarYear;
     }
+    /**
+     * @brief getLunarDay 获取阴历日信息
+     * @return 阴历日信息字符串
+     */
     QString getLunarDay()const
     {
         return m_LunarDay;
     }
+    /**
+     * @brief getLunarData 更新当天的农历年份数据。
+     */
     void getLunarData();
 protected:
+    /**
+     * @brief mousePressEvent 鼠标单击日期区域外隐藏日程浮框
+     * @param event 鼠标单击事件
+     */
     void mousePressEvent(QMouseEvent *event) override;
 signals:
+    /**
+     * @brief signaldoubleclickDate 鼠标双击的信号
+     * @param date 传递日期参数
+     */
     void signaldoubleclickDate(QDate date);
+    /**
+     * @brief signalselectWeekwindow 选择周视图的信号
+     * @param date 传递日期参数
+     */
     void signalselectWeekwindow(QDate date);
+    /**
+     * @brief signalselectMonth 选择月视图的信号
+     * @param date 传递日期参数
+     */
     void signalselectMonth(QDate date);
+    /**
+     * @brief signalUpdateYearDate 更新年视图的信号
+     * @param date 传递日期参数
+     */
     void signalUpdateYearDate(const QDate &date);
+    /**
+     * @brief signalupdateschcedule 更新日程的信号
+     */
     void signalupdateschcedule();
 public slots:
+    /**
+     * @brief slotHideInfo 隐藏日程浮框
+     */
     void slotHideInfo();
+    /**
+     * @brief slotSelectInfo 设置是否选择了日期的标志
+     * @param flag 是否选择了如期
+     */
     void slotSelectInfo(bool flag);
+    /**
+     * @brief slotupdateSchedule 更新日程
+     * @param id
+     */
     void slotupdateSchedule(const int id);
+    /**
+     * @brief slotSetSchceduleHide 隐藏日程浮框
+     */
     void slotSetSchceduleHide();
+    /**
+     * @brief slotcurrentDateChanged 设置当前时间
+     * @param date 时间
+     */
     void slotcurrentDateChanged(QDate date);
 private:
     QList<CYearView *>          m_monthViewList;
