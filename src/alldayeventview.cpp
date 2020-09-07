2645109bc5694d7f2687152e95aa7e0bdf64d82a
chenhaifeng <chenhaifeng@uniontech.com>
2020-09-07 14:23:40 +0800

    fix: bug45968使用笔记本触控板双指滑动时，日程标签可左右移动
    在滚轮事件中，对滚轮的方向进行判断，若为左右方向则退出
    
    Log: 使用笔记本触控板双指滑动时，日程标签可左右移动
    
    link: https://pms.uniontech.com/zentao/bug-view-45968.html
    Change-Id: Ifc91099a01c2669e28b136d45838e4e09419fcdc
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/dde-calendar/+/3840
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: cuizhen <cuizhen@uniontech.com>
    Reviewed-by: chenhaifeng <chenhaifeng@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/alldayeventview.cpp
+++ src/alldayeventview.cpp
@@ -484,6 +484,10 @@ void CAllDayEventWeekView::mouseDoubleClickEvent(QMouseEvent *event)
 
 void CAllDayEventWeekView::wheelEvent(QWheelEvent *event)
 {
+    //若滚轮事件为左右方向则退出
+    if(event->orientation() == Qt::Orientation::Horizontal){
+        return;
+    }
     emit signalScheduleShow(false);
     DGraphicsView::wheelEvent(event);
 }
