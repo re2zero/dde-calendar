0af9570005f582e44d9d17dc997626644b4295e6
chenhaifeng <chenhaifeng@uniontech.com>
2020-09-22 15:33:36 +0800

    chore: 修改回复语,添加注释删除无用代码
    修改回复语，添加注释删除无用代码
    
    Log: 修改回复语，添加注释删除无用代码
    
    link:
    Change-Id: I52c223593494ff5c3cacb16dccc6deaa7ba91073
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/dde-calendar/+/5859
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: luzhou <luzhou@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- schedule-plugin/src/globaldef.h
+++ schedule-plugin/src/globaldef.h
@@ -40,11 +40,16 @@
 
 #define PROCESS_OPEN_CALENDAR "dbus-send --print-reply --dest=com.deepin.Calendar /com/deepin/Calendar com.deepin.Calendar.RaiseWindow"
 
-//dbus
+//日程重复类型
+//每天
 #define DBUS_RRUL_EVED "FREQ=DAILY"
+//每周
 #define DBUS_RRUL_EVEW "FREQ=WEEKLY"
+//每月
 #define DBUS_RRUL_EVEM "FREQ=MONTHLY"
+//每年
 #define DBUS_RRUL_EVEY "FREQ=YEARLY"
+//每个工作日
 #define DBUS_RRUL_WORK "FREQ=DAILY;BYDAY=MO,TU,WE,TH,FR"
 
 //日程json关键字
@@ -54,7 +59,7 @@
 #define JSON_CHANGE "CHANGE"
 #define JSON_NO_INTENT "NO_INTENT"
 #define JSON_YES_INTENT "YES_INTENT"
-
+//日程json槽位关键字
 #define JSON_NAME "name"
 #define JSON_CONTENT "content"
 #define JSON_REPEAT "repeat"
@@ -73,7 +78,7 @@
 
 #define JOSN_DATE_DELIMITER "/"
 #define JSON_DATETIME_DELIMITER "T"
-
+//每天多少秒
 #define ONE_DAY_SECS 60 * 60 * 24
 
 //widget show
@@ -86,61 +91,68 @@
 #define CHANGE_ALL_BUTTON_STRING "修改全部"
 #define CHANGE_ONLYL_BUTTON_STRING "仅修改此日程"
 
-#define SHOW_SCHEDULE_MAX_COUNT 10
 
 #define CREATE_ERR_TTS "新建功能不支持此说法"
 #define CANCEL_ERR_TTS "查询功能不支持此说法"
-#define QUERY_ERR_TTS "您还没有日程提醒，无法寻找对应的日程"
-#define CANCEL_SELECT_STATE_ERR_TTS "我听不懂你在说什么。请问您要删除哪一个日程"
-#define INQUIRY_STATE_ERR_TTS "请问您要选择哪一个日程"
-#define CHANGE_TO_TTS "请问您要将此日程修改到什么时候？"
-
-#define G_ERR_TTS "我听不懂你在说什么"
-#define CANCEL_DELETION_TTS "已为您取消删除日程"
-#define CONFIRM_DELETION_TTS "已为您删除日程"
-
-#define CANCEL_CHANGE_TTS "已为您取消修改日程"
-#define CONFIRM_CHANGE_TTS "已为您修改日程"
-
-#define NO_SCHEDULE_TTS "您还没有日程提醒"
+//没有对应日程的回复语
+#define QUERY_ERR_TTS "您还没有日程提醒，无法寻找对应的日程！"
+//修改日程询问回复语
+#define CHANGE_TO_TTS "请问您要将此日程修改到什么时间或修改到什么内容？"
+//错误操作回复语
+#define G_ERR_TTS "当前场景不支持这个操作哦。"
+//取消删除日程回复语
+#define CANCEL_DELETION_TTS "已为您取消删除日程。"
+//确认删除日程回复语
+#define CONFIRM_DELETION_TTS "已为您删除日程。"
+//取消修改日程回复语
+#define CANCEL_CHANGE_TTS "已为您取消修改日程。"
+//确认修改日程回复语
+#define CONFIRM_CHANGE_TTS "已为您修改日程。"
+//没有日程提醒回复语
+#define NO_SCHEDULE_TTS "您还没有日程提醒。"
+//删除重复日程询问回复语
 #define REPEST_SCHEDULE_CANCEL_TTS "该日程是循环的日程，请问是删除当前日程还是所有日程？"
-#define REPEST_SCHEDULE_OFFSET_ERR_TTS "没有找到对应的日程"
+//删除普通日程询问回复语
 #define CONFIRM_SCHEDULE_CANCEL_TTS "我要帮您取消这个日程吗？操作后将在日历中同步删除。"
-#define CANCEL_SELECT_TTS "这是您的所有日程，您要删除哪一个?"
-
-#define SELECT_INQUIRY_CHANGE_TTS "这是您的所有日程，您要将哪个日程修改到到什么时间或修改到什么内容？"
+//删除选择日程回复语
+#define CANCEL_SELECT_TTS "这是您的所有日程，您要删除哪一个日程?"
+//选择修改回复语
 #define SELECT_CHANGE_TTS "这是您的所有日程，您要修改哪一个日程？"
-#define INQUIRY_CHANGE_TTS "您要将该日程修改到到什么时间或修改到什么内容？"
+//修改重复日程询问回复语
 #define REPEST_SCHEDULE_CHANGE_TTS "该日程是循环的日程，请问是修改当前日程还是所有日程？"
+//修改普通日程询问回复语
 #define CONFIRM_SCHEDULE_CHANGE_TTS "我要帮您修改这个日程吗？操作后将在日历中同步修改。"
 #define ALL_DAY "全天"
-#define OVERTIME_TTS "只能取消或修改未来半年的日程"
-
-#define MAXIMUM_DAYS_IN_THE_FUTURE 180
+//修改或取消时间不在未来半年的回复语
+#define OVERTIME_TTS "只能取消或修改未来半年的日程！"
+//半年天数
+#define MAXIMUM_DAYS_IN_THE_FUTURE 183
+//不带窗口的回复宏定义
 #define REPLY_ONLY_TTS(reply, msgtts, dsptts, isEnd) \
     reply.setReplyType(Reply::RT_STRING_TTS | Reply::RT_STRING_DISPLAY); \
     reply.ttsMessage(msgtts); \
     reply.displayMessage(dsptts); \
     reply.setShouldEndSession(isEnd); \
-    //
 
+//带窗口的回复宏定义
 #define REPLY_WIDGET_TTS(reply, widget, msgtts, dsptts, isEnd) \
     reply.setReplyType(Reply::RT_INNER_WIDGET | Reply::RT_STRING_TTS | Reply::RT_STRING_DISPLAY); \
     reply.setReplyWidget(widget); \
     reply.ttsMessage(msgtts); \
     reply.displayMessage(dsptts); \
     reply.setShouldEndSession(isEnd); \
-    //
 
+//未来半年开始时间和结束时间
 #define TIME_FRAME_IN_THE_NEXT_SIX_MONTHT \
     QDateTime beginTime = QDateTime::currentDateTime(); \
     QDateTime endTime = QDateTime::currentDateTime().addDays(180);
 
 //modifyScheduleItem
 #define MODIFY_ITEM_HEIGHT 48
-
+//item圆角
 #define ITEM_RADIUS 8
 
+//最多显示10个item
 #define ITEM_SHOW_NUM 10
 
 #define OPENCALENDAR_WIDGET_TITLE "在“日历”中查看所有%1个结果..."
