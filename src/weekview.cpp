8b6ce161f65c3cf0e82e44ae30a06cf7d440b5e2
zhengxiaokang <zhengxiaokang@uniontech.com>
2020-09-27 16:59:04 +0800

    chore: 为weekview添加注释信息
    添加注释信息
    
    Log: 添加注释信息
    
    link:
    Change-Id: Icb8215aca2ac9f2b282746b2ea8de41336e6d873
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/dde-calendar/+/6446
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: chenhaifeng <chenhaifeng@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/weekview.cpp
+++ src/weekview.cpp
@@ -34,29 +34,41 @@
 #include <QMouseEvent>
 
 DWIDGET_USE_NAMESPACE
-
+/**
+ * @brief CWeekView 构造函数
+ * @param parent 父类
+ */
 CWeekView::CWeekView(QWidget *parent)
     : QWidget(parent)
 {
+    //设置字体大小
     m_dayNumFont.setPixelSize(DDECalendar::FontSizeSixteen);
+    //设置字体权重
     m_dayNumFont.setWeight(QFont::Light);
     // cells grid
     QHBoxLayout *hboxLayout = new QHBoxLayout;
     hboxLayout->setMargin(0);
     hboxLayout->setSpacing(0);
-
+    //显示周数的widget
     for (int c = 0; c != DDEWeekCalendar::NumWeeksDisplayed; ++c) {
         QWidget *cell = new QWidget;
+        //设置大小
         cell->setFixedSize(DDEWeekCalendar::WWeekCellWidth, DDEWeekCalendar::WWeekCellHeight);
+        //设置事件过滤器
         cell->installEventFilter(this);
         cell->setFocusPolicy(Qt::ClickFocus);
+        //添加到布局中
         hboxLayout->addWidget(cell);
         m_cellList.append(cell);
     }
+    //设置布局
     setLayout(hboxLayout);
+    //设置最小宽度
     setMinimumWidth(150);
 }
-
+/**
+  * @brief ~CWeekView 析构函数
+  */
 CWeekView::~CWeekView()
 {
     for (int i = 0; i < DDEWeekCalendar::NumWeeksDisplayed; i++) {
@@ -65,13 +77,19 @@ CWeekView::~CWeekView()
     }
     m_cellList.clear();
 }
-
+/**
+ * @brief setFirstWeekDay 设置每周的第一天是周几，并更新
+ * @param weekday 周几
+ */
 void CWeekView::setFirstWeekDay(int weekday)
 {
     m_firstWeekDay = weekday;
     updateDate();
 }
-
+/**
+ * @brief setCurrentDate 设置当天时间，并更新
+ * @param date 时间
+ */
 void CWeekView::setCurrentDate(const QDate date)
 {
     m_selectDate = date;
@@ -79,32 +97,55 @@ void CWeekView::setCurrentDate(const QDate date)
     updateDate();
     update();
 }
-
+/**
+ * @brief setTheMe 根据系统主题类型设置颜色
+ * @param type 系统主题类型
+ */
 void CWeekView::setTheMe(int type)
 {
     m_themetype = type;
-
+    //根据系统主题类型设置颜色
     if (type == 0 || type == 1) {
+        //系统主题类型为浅色或跟随系统
+        //默认字体颜色
         m_defaultTextColor = Qt::black;
+        //默认背景色
         m_backgrounddefaultColor = Qt::white;
+        //默认当前日期字体颜色
         m_currentDayTextColor = Qt::white;
+        //系统活动色
         m_backgroundcurrentDayColor = CScheduleDataManage::getScheduleDataManage()->getSystemActiveColor();
+        //背景色
         m_fillColor = "#FFFFFF";
     } else if (type == 2) {
+        //系统主题类型为神色
+        //设置默认字体颜色
         m_defaultTextColor = "#C0C6D4";
+        //设置默认背景色
         m_backgrounddefaultColor = "#FFFFFF";
+        //设置透明度
         m_backgrounddefaultColor.setAlphaF(0.05);
+        //设置当前日期字体颜色
         m_currentDayTextColor = "#B8D3FF";
+        //系统活动色
         m_backgroundcurrentDayColor = CScheduleDataManage::getScheduleDataManage()->getSystemActiveColor();
+        //设置背景色
         m_fillColor = "#000000";
+        //透明度
         m_fillColor.setAlphaF(0.05);
     }
 }
-
+/**
+ * @brief setsearchfalg
+ * @param flag
+ */
 void CWeekView::setsearchfalg(bool flag)
 {
     m_searchfalg = flag;
 }
+/**
+ * @brief slotprev 选择上一周
+ */
 void CWeekView::slotprev()
 {
     if (m_selectDate.year() >= DDECalendar::QueryEarliestYear) {
@@ -113,15 +154,23 @@ void CWeekView::slotprev()
     } else {
     }
 }
-
+/**
+ * @brief slotnext 选择下一周
+ */
 void CWeekView::slotnext()
 {
     QDate date  = m_selectDate.addDays(8);;
     setCurrentDate(date);
 }
+/**
+ * @brief paintCell 绘制周数
+ * @param cell 周数所在的widget
+ */
 void CWeekView::paintCell(QWidget *cell)
 {
+    //矩形
     const QRect rect(0, 0, cell->width(), cell->height());
+    //cell所在的索引
     const int pos = m_cellList.indexOf(cell);
     //计算当前日期周数
     int weekNumber = QDate::currentDate().weekNumber();
@@ -129,58 +178,75 @@ void CWeekView::paintCell(QWidget *cell)
     if (QDate::currentDate().dayOfWeek() == DDEWeekCalendar::AFewDaysofWeek) {
         ++weekNumber;
     }
-
+    //是否是今天
     const bool isCurrentDay = m_days[pos].addDays(3).weekNumber() == weekNumber &&
                               m_days[pos].addDays(3).year() == QDate::currentDate().year();
+    //是否是选择的周数
     const bool isSelectDay = m_days[pos].addDays(m_weekAddDay).weekNumber() == m_selectDate.addDays(m_weekAddDay).weekNumber();
-
+    //绘制cell
     QPainter painter(cell);
-
     painter.save();
     painter.setRenderHints(QPainter::HighQualityAntialiasing);
     painter.setBrush(QBrush(m_fillColor));
     painter.setPen(Qt::NoPen);
-    painter.drawRect(rect);//画矩形
+    //画矩形
+    painter.drawRect(rect);
     painter.restore();
     painter.setPen(Qt::SolidLine);
 
     const QString dayNum = QString::number(m_days[pos].addDays(3).weekNumber());
-
+    //早于1900年第一周，return
     if (m_days[pos].year() < DDECalendar::QueryEarliestYear && dayNum != "1")
         return;
 
     if (isSelectDay) {
+        //选择周数
         QRect fillRect((cell->width() - 24) / 2, (cell->height() - 32) / 2 + 4, 24, 24);
         painter.save();
         painter.setRenderHint(QPainter::Antialiasing);
         painter.setBrush(QBrush(CScheduleDataManage::getScheduleDataManage()->getSystemActiveColor()));
         painter.setPen(Qt::NoPen);
+        //绘制背景圆
         painter.drawEllipse(fillRect);
         painter.restore();
         painter.setRenderHint(QPainter::HighQualityAntialiasing);
+        //设置文字颜色
         painter.setPen(m_currentDayTextColor);
+        //设置字体
         painter.setFont(m_dayNumFont);
+        //绘制周数
         painter.drawText(QRect(0, 0, cell->width(), cell->height()), Qt::AlignCenter, dayNum);
     } else {
         if (isCurrentDay) {
+            //设置今天的背景色
             painter.setPen(m_backgroundcurrentDayColor);
         } else {
+            //设置默认的背景色
             painter.setPen(m_defaultTextColor);
         }
+        //设置字体
         painter.setFont(m_dayNumFont);
+        //绘制周数
         painter.drawText(QRect(0, 0, cell->width(), cell->height()), Qt::AlignCenter, dayNum);
     }
     painter.end();
 }
-
+/**
+ * @brief eventFilter 过滤器
+ * @param o 事件对象
+ * @param e 事件类型
+ * @return  false
+ */
 bool CWeekView::eventFilter(QObject *o, QEvent *e)
 {
     QWidget *cell = qobject_cast<QWidget *>(o);
 
     if (cell && m_cellList.contains(cell)) {
         if (e->type() == QEvent::Paint) {
+            //绘图事件
             paintCell(cell);
         } else if (e->type() == QEvent::MouseButtonPress) {
+            //鼠标左击事件
             QMouseEvent *mouseEvent = dynamic_cast<QMouseEvent *>(e);
             if (mouseEvent->button() ==Qt::LeftButton) {
                 cellClicked(cell);
@@ -190,56 +256,70 @@ bool CWeekView::eventFilter(QObject *o, QEvent *e)
 
     return false;
 }
-
+/**
+ * @brief cellClicked 选择点击的日期
+ * @param cell 周数所在的widget
+ */
 void CWeekView::cellClicked(QWidget *cell)
 {
     if (!m_cellSelectable)
         return;
-
+    //鼠标点击的索引
     const int pos = m_cellList.indexOf(cell);
-
+    //没有return
     if (pos == -1)
         return;
-
+    //设置选择的cell
     setSelectedCell(pos);
     update();
 }
-
+/**
+ * @brief setSelectedCell 设置被选择的周数
+ * @param index 周数所在的索引
+ */
 void CWeekView::setSelectedCell(int index)
 {
     if (m_selectedCell == index)
         return;
-
+    //选择的索引
     const int prevPos = m_selectedCell;
     m_selectedCell = index;
-
+    //更新
     m_cellList.at(prevPos)->update();
     m_cellList.at(index)->update();
+    //设置选择的日期
     m_selectDate = m_days[index];
     const QString dayNum = QString::number(m_days[index].addDays(3).weekNumber());
-
+    //早于1900年第一周，return
     if (m_days[index].year() < DDECalendar::QueryEarliestYear && dayNum != "1")
         return;
 
     emit signalsSelectDate(m_days[index], m_days[index].addDays(m_weekAddDay));
 }
-
+/**
+ * @brief updateDate 更新数据
+ */
 void CWeekView::updateDate()
 {
     m_weekAddDay = (m_selectDate.dayOfWeek() + m_firstWeekDay) % DDEWeekCalendar::AFewDaysofWeek;
     QDate weekfirstDay = m_selectDate.addDays(-m_weekAddDay);
     m_days[4] = weekfirstDay;
-
+    //设置前四个周数
     for (int i(0); i < 4; ++i) {
         m_days[i] = weekfirstDay.addDays(-(4 - i) * DDEWeekCalendar::AFewDaysofWeek);
     }
+    //设置后五个周数
     for (int i(5); i != DDEWeekCalendar::NumWeeksDisplayed; ++i) {
         m_days[i] = weekfirstDay.addDays((i - 4) * DDEWeekCalendar::AFewDaysofWeek);
     }
-
+    //默认设置选择第四个
     setSelectedCell(4);
     update();
 }
+/**
+ * @brief resizeEvent 调整窗口大小
+ * @param event 窗口大小调整事件
+ */
 void CWeekView::resizeEvent(QResizeEvent *event)
 {
     int w = width() / DDEWeekCalendar::NumWeeksDisplayed;
@@ -276,6 +356,10 @@ void CWeekView::resizeEvent(QResizeEvent *event)
     }
     QWidget::resizeEvent(event);
 }
+/**
+ * @brief wheelEvent 鼠标滚轮切换上一周下一周
+ * @param event 鼠标滚轮事件
+ */
 void CWeekView::wheelEvent(QWheelEvent *event)
 {
     Q_UNUSED(event);
@@ -285,8 +369,10 @@ void CWeekView::wheelEvent(QWheelEvent *event)
     //判断是否是拖拽状态
     if (!isDragging) {
         if (event->delta() < 0) {
+            //上一周
             slotnext();
         } else {
+            //下一周
             slotprev();
         }
     }
--- src/weekview.h
+++ src/weekview.h
@@ -22,53 +22,22 @@
 #include <QObject>
 #include <QWidget>
 #include <QDate>
-
+/**
+ * @brief The CWeekView class
+ */
 class CWeekView : public QWidget
 {
     Q_OBJECT
 public:
-    /**
-     * @brief CWeekView 构造函数
-     * @param parent 父类
-     */
     explicit CWeekView(QWidget *parent = nullptr);
-    /**
-      * @brief ~CWeekView 析构函数
-      */
     ~CWeekView();
-    /**
-     * @brief setFirstWeekDay 设置每周的第一天是周几，并更新
-     * @param weekday 周几
-     */
     void setFirstWeekDay(int weekday);
-    /**
-     * @brief setCurrentDate 设置当天时间，并更新
-     * @param date 时间
-     */
     void setCurrentDate(const QDate date);
-    /**
-     * @brief setTheMe 根据系统主题类型设置颜色
-     * @param type 系统主题类型
-     */
     void setTheMe(int type = 0);
-    /**
-     * @brief setsearchfalg
-     * @param flag
-     */
     void setsearchfalg(bool flag);
 public slots:
-    /**
-     * @brief cellClicked 选择点击的日期
-     * @param cell 周数所在的widget
-     */
     void cellClicked(QWidget *cell);
-    /**
-     * @brief slotprev 选择上一周
-     */
     void slotprev();
-    /**
-     * @brief slotnext 选择下一周
-     */
     void slotnext();
 signals:
     /**
@@ -83,55 +52,46 @@ signals:
      */
     void signalIsDragging(bool &isDragging);
 protected:
-    /**
-     * @brief resizeEvent 调整窗口大小
-     * @param event 窗口大小调整事件
-     */
     void resizeEvent(QResizeEvent *event) override;
-    /**
-     * @brief wheelEvent 鼠标滚轮切换上一周下一周
-     * @param event 鼠标滚轮事件
-     */
     void wheelEvent(QWheelEvent *event) override;
-
 private:
-    /**
-     * @brief paintCell 绘制周数
-     * @param cell 周数所在的widget
-     */
     void paintCell(QWidget *cell);
-    /**
-     * @brief eventFilter 过滤器
-     * @param o 事件对象
-     * @param e 事件类型
-     * @return  false
-     */
     bool eventFilter(QObject *o, QEvent *e) override;
-    /**
-     * @brief setSelectedCell 设置被选择的周数
-     * @param index 周数所在的索引
-     */
     void setSelectedCell(int index);
-    /**
-     * @brief updateDate 更新数据
-     */
     void updateDate();
 private:
+    /**
+     * @brief m_cellList list
+     *
+     * 周数list
+     */
     QList<QWidget *> m_cellList;
+    //选择的日期
     QDate m_selectDate;
+    //周数对应的日期
     QDate m_days[10];
+    //是否被选择
     bool m_cellSelectable = true;
+    //选择的索引
     int m_selectedCell = 0;
+    //字体
     QFont m_dayNumFont;
-
+    //默认字体颜色
     QColor m_defaultTextColor = Qt::black;
+    //默认背景色
     QColor m_backgrounddefaultColor = Qt::white;
+    //当天的字体颜色
     QColor m_currentDayTextColor = Qt::white;
+    //今天的背景色
     QColor m_backgroundcurrentDayColor = "#0081FF";
+    //背景色
     QColor m_fillColor = Qt::white;
+    //每周起始的周数
     int m_firstWeekDay;
     int m_weekAddDay = 0;
+    //系统主题类型
     int m_themetype = 1;
+    //选择标志
     bool m_searchfalg = false;
 };
 
