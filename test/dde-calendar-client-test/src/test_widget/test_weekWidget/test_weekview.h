8fd0a559ae50db9908779b77e57547fc29761c2b
zhengxiaokang <zhengxiaokang@uniontech.com>
2021-01-14 16:08:02 +0800

    chore: 为weekview文件添加单元测试用例
    为weekview文件添加单元测试用例
    
    Log: 为weekview文件添加单元测试用例
    
    link:
    Change-Id: Ib97bf78630965a11d55ba77e16e37c1263fc7a80

---
+++ test/dde-calendar-client-test/src/test_widget/test_weekWidget/test_weekview.h
@@ -0,0 +1,39 @@
+/*
+* Copyright (C) 2019 ~ 2021 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     zhengxiaokang <zhengxiaokang@uniontech.com>
+*
+* Maintainer: zhengxiaokang <zhengxiaokang@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef TEST_WEEKVIEW_H
+#define TEST_WEEKVIEW_H
+
+#include "weekWidget/weekview.h"
+#include "gtest/gtest.h"
+#include <QObject>
+
+class test_weekview : public QObject
+    , public ::testing::Test
+{
+public:
+    test_weekview();
+    ~test_weekview();
+
+protected:
+    CWeekView *mWeekView = nullptr;
+};
+
+#endif // TEST_WEEKVIEW_H
