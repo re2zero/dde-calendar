929376ea04535e3a589df8b275ea8b1be27a3b6f
zhengxiaokang <zhengxiaokang@uniontech.com>
2021-01-14 13:22:35 +0800

    chore: 为schedulesearchview文件添加单元测试用例
    为schedulesearchview文件添加单元测试用例
    
    Log: 为schedulesearchview文件添加单元测试用例
    
    link:
    Change-Id: I88a6c19ac05222ae14df686783cb36e02c3b4155

---
+++ test/dde-calendar-client-test/src/test_widget/test_schedulesearchview.h
@@ -0,0 +1,42 @@
+/*
+* Copyright (C) 2019 ~ 2021 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     zhengxiaokang <zhengxiaokang@uniontech.com>
+*
+* Maintainer: zhengxiaokang <zhengxiaokang@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef TEST_SCHEDULESEARCHVIEW_H
+#define TEST_SCHEDULESEARCHVIEW_H
+
+#include "widget/schedulesearchview.h"
+#include "gtest/gtest.h"
+#include <QObject>
+
+class test_schedulesearchview : public QObject
+    , public ::testing::Test
+{
+public:
+    test_schedulesearchview();
+    ~test_schedulesearchview();
+
+protected:
+    CScheduleSearchView *mScheduleSearchView = nullptr;
+    CScheduleSearchDateItem *mScheduleSearchDateItem = nullptr;
+    CScheduleListWidget *mScheduleListWidget = nullptr;
+    CScheduleSearchItem *mScheduleSearchItem = nullptr;
+};
+
+#endif // TEST_SCHEDULESEARCHVIEW_H
