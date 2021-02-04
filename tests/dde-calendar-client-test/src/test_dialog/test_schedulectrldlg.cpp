8b75211b8ad7fc46dd559a32dfc1e5bcdf7745a1
zhengxiaokang <zhengxiaokang@uniontech.com>
2021-02-04 14:41:30 +0800

    chore: 为schedulectrldlg文件添加单元测试用例
    为schedulectrldlg文件添加单元测试用例
    
    Log: 为schedulectrldlg文件添加单元测试用例
    
    Change-Id: I8de613df2ad736b58daead0490159ebc0f8b24dc

---
+++ tests/dde-calendar-client-test/src/test_dialog/test_schedulectrldlg.cpp
@@ -0,0 +1,84 @@
+/*
+   * Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+   *
+   * Author:     chenhaifeng <chenhaifeng@uniontech.com>
+   *
+   * Maintainer: chenhaifeng <chenhaifeng@uniontech.com>
+   *
+   * This program is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * any later version.
+   *
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+#include "test_schedulectrldlg.h"
+
+test_schedulectrldlg::test_schedulectrldlg()
+{
+    mScheduleCtrlDlg = new CScheduleCtrlDlg();
+}
+
+test_schedulectrldlg::~test_schedulectrldlg()
+{
+    delete mScheduleCtrlDlg;
+    mScheduleCtrlDlg = nullptr;
+}
+
+//void CScheduleCtrlDlg::setTheMe(const int type)
+TEST_F(test_schedulectrldlg, setTheMe)
+{
+    mScheduleCtrlDlg->setTheMe(1);
+    mScheduleCtrlDlg->setTheMe(2);
+}
+
+//void CScheduleCtrlDlg::buttonJudge(int id)
+TEST_F(test_schedulectrldlg, buttonJudge)
+{
+    mScheduleCtrlDlg->buttonJudge(1);
+}
+
+//QAbstractButton *CScheduleCtrlDlg::addPushButton(QString btName, bool type)
+TEST_F(test_schedulectrldlg, addPushButton)
+{
+    mScheduleCtrlDlg->addPushButton("schedule", true);
+    mScheduleCtrlDlg->addPushButton("schedule", false);
+}
+
+//QAbstractButton *CScheduleCtrlDlg::addsuggestButton(QString btName, bool type)
+TEST_F(test_schedulectrldlg, addsuggestButton)
+{
+    mScheduleCtrlDlg->addsuggestButton("schedule", true);
+    mScheduleCtrlDlg->addsuggestButton("schedule", false);
+}
+
+//QAbstractButton *CScheduleCtrlDlg::addWaringButton(QString btName, bool type)
+TEST_F(test_schedulectrldlg, addWaringButton)
+{
+    mScheduleCtrlDlg->addsuggestButton("schedule", true);
+    mScheduleCtrlDlg->addsuggestButton("schedule", false);
+}
+
+//void CScheduleCtrlDlg::setText(QString str)
+TEST_F(test_schedulectrldlg, setText)
+{
+    mScheduleCtrlDlg->setText("schedule");
+}
+
+//void CScheduleCtrlDlg::setInformativeText(QString str)
+TEST_F(test_schedulectrldlg, setInfomativeText)
+{
+    mScheduleCtrlDlg->setInformativeText("schedule");
+}
+
+//int CScheduleCtrlDlg::clickButton()
+TEST_F(test_schedulectrldlg, clickButton)
+{
+    mScheduleCtrlDlg->clickButton();
+}
