87f29898370ed383c4e9be6a22c8437df63fa2b8
zhengxiaokang <zhengxiaokang@uniontech.com>
2021-02-07 13:45:24 +0800

    chore: 添加单元测试用例
    添加单元测试用例
    
    Log: 添加单元测试用例
    Change-Id: I723a5fced19fd9ab22bf361f068dd7980c4b68a7

---
+++ tests/dde-calendar-client-test/src/test_customWidget/test_todaybutton.h
@@ -0,0 +1,37 @@
+/*
+   * Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+   *
+   * Author:     chenhaifeng <chenhaifeng@uniontech.com>
+   *
+   * Maintainer: chenhaifeng <chenhaifeng@uniontech.com>
+   *
+   * This program is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * any later version.
+   *
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+#ifndef TEST_TODAYBUTTON_H
+#define TEST_TODAYBUTTON_H
+
+#include <QObject>
+#include <gtest/gtest.h>
+#include "customWidget/todaybutton.h"
+
+class test_todaybutton : public::QObject, public::testing::Test
+{
+public:
+    test_todaybutton();
+    ~test_todaybutton();
+protected:
+    CTodayButton *mTodayButton = nullptr;
+};
+
+#endif // TEST_TODAYBUTTON_H
