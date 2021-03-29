c0ee881ece77171f6bdaf8ee6f7bf78b247230b4
chenhaifeng <chenhaifeng@uniontech.com>
2021-03-29 11:41:27 +0800

    fix: 修复在某些情况下可以打开多个应用的问题
    修复在某些情况下可以打开多个应用的问题
    
    Log: 修复在某些情况下可以打开多个应用的问题
    Change-Id: Iff9a1732ffe12b7e924472b0ddca004dd55af7e0

--- calendar-client/src/main.cpp
+++ calendar-client/src/main.cpp
@@ -46,65 +46,59 @@ int main(int argc, char *argv[])
     app = DApplication::globalApplication(argc, argv);
 #endif
 
-    ////获取设置是否为平板模式
-    //#if (DTK_VERSION >= DTK_VERSION_CHECK(5, 4, 7, 0))
-    //    TabletConfig::setIsTablet(DGuiApplicationHelper::isTabletEnvironment());
-    //#endif
-
-    QAccessible::installFactory(accessibleFactory);
-    app->setOrganizationName("deepin");
-    app->setApplicationName("dde-calendar");
-    app->loadTranslator();
-    app->setApplicationVersion(VERSION);
-    // meta information that necessary to create the about dialog.
-    app->setProductName(QApplication::translate("CalendarWindow", "Calendar"));
-    QIcon t_icon = QIcon::fromTheme("dde-calendar");
-    app->setProductIcon(t_icon);
-    app->setApplicationDescription(QApplication::translate("CalendarWindow", "Calendar is a tool to view dates, and also a smart daily planner to schedule all things in life. "));
-    app->setApplicationAcknowledgementPage("https://www.deepin.org/acknowledgments/dde-calendar");
-    //命令行参数
-    QCommandLineParser _commandLine;       //建立命令行解析
-    _commandLine.addHelpOption();          //增加-h/-help解析命令
-    _commandLine.addVersionOption();       //增加-v 解析命令
-    _commandLine.setSingleDashWordOptionMode(QCommandLineParser::ParseAsLongOptions);
-    _commandLine.process(*app);
-//如果dtk版本为5.2.0.1以上则使用新的dtk接口
+    app->setAutoActivateWindows(true);
+    //如果dtk版本为5.2.0.1以上则使用新的dtk接口
 #if (DTK_VERSION > DTK_VERSION_CHECK(5, 2, 0, 1))
     //设置为-1将永久等待
     DGuiApplicationHelper::setSingleInstanceInterval(-1);
 #endif
-    if (!DGuiApplicationHelper::instance()->setSingleInstance(
-                app->applicationName(),
-                DGuiApplicationHelper::UserScope)) {
-        qDebug() << "there's an dde-calendar instance running.";
-        QProcess::execute("dbus-send --print-reply --dest=com.deepin.Calendar "
-                          "/com/deepin/Calendar com.deepin.Calendar.RaiseWindow");
-        return 0;
+
+    if (DGuiApplicationHelper::setSingleInstance(app->applicationName(), DGuiApplicationHelper::UserScope)) {
+        QAccessible::installFactory(accessibleFactory);
+        app->setOrganizationName("deepin");
+        app->setApplicationName("dde-calendar");
+        app->loadTranslator();
+        app->setApplicationVersion(VERSION);
+        // meta information that necessary to create the about dialog.
+        app->setProductName(QApplication::translate("CalendarWindow", "Calendar"));
+        QIcon t_icon = QIcon::fromTheme("dde-calendar");
+        app->setProductIcon(t_icon);
+        app->setApplicationDescription(QApplication::translate("CalendarWindow", "Calendar is a tool to view dates, and also a smart daily planner to schedule all things in life. "));
+        app->setApplicationAcknowledgementPage("https://www.deepin.org/acknowledgments/dde-calendar");
+        //命令行参数
+        QCommandLineParser _commandLine; //建立命令行解析
+        _commandLine.addHelpOption(); //增加-h/-help解析命令
+        _commandLine.addVersionOption(); //增加-v 解析命令
+        _commandLine.setSingleDashWordOptionMode(QCommandLineParser::ParseAsLongOptions);
+        _commandLine.process(*app);
+
+        app->setAutoActivateWindows(true);
+        CConfigSettings::init();
+        bool isOk = false;
+        int viewtype = CConfigSettings::value("base.view").toInt(&isOk);
+        if (!isOk)
+            viewtype = 2;
+        DLogManager::registerConsoleAppender();
+        DLogManager::registerFileAppender();
+        //获取应用配置
+        DApplicationSettings applicationset(app);
+        //为了与老版本配置兼容
+        Calendarmainwindow ww(viewtype - 1);
+        ExportedInterface einterface(&ww);
+        einterface.registerAction("CREATE", "create a new schedule");
+        einterface.registerAction("VIEW", "check a date on calendar");
+        einterface.registerAction("QUERY", "find a schedule information");
+        einterface.registerAction("CANCEL", "cancel a schedule");
+        QDBusConnection dbus = QDBusConnection::sessionBus();
+        dbus.registerService("com.deepin.Calendar");
+        dbus.registerObject("/com/deepin/Calendar", &ww);
+        ww.slotTheme(DApplicationHelper::instance()->themeType());
+        ww.show();
+        PERF_PRINT_END("POINT-01");
+        return app->exec();
     }
 
-    app->setAutoActivateWindows(true);
-    CConfigSettings::init();
-    bool isOk = false;
-    int viewtype = CConfigSettings::value("base.view").toInt(&isOk);
-    if (!isOk)
-        viewtype = 2;
-    DLogManager::registerConsoleAppender();
-    DLogManager::registerFileAppender();
-    //获取应用配置
-    DApplicationSettings applicationset(app);
-    //为了与老版本配置兼容
-    Calendarmainwindow ww(viewtype - 1);
-    ExportedInterface einterface(&ww);
-    einterface.registerAction("CREATE", "create a new schedule");
-    einterface.registerAction("VIEW", "check a date on calendar");
-    einterface.registerAction("QUERY", "find a schedule information");
-    einterface.registerAction("CANCEL", "cancel a schedule");
-    QDBusConnection dbus = QDBusConnection::sessionBus();
-    dbus.registerService("com.deepin.Calendar");
-    dbus.registerObject("/com/deepin/Calendar", &ww);
-    ww.slotTheme(DApplicationHelper::instance()->themeType());
-    ww.show();
-
     PERF_PRINT_END("POINT-01");
-    return app->exec();
+
+    return 0;
 }
