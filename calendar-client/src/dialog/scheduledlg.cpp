3aafa745158fa573c27911b819d2945852e54a9b
chenhaifeng <chenhaifeng@uniontech.com>
2022-04-12 15:55:30 +0800

    feat: 创建农历日程对重复为每年的闰月添加提醒
    创建农历日程对重复为每年的闰月添加提醒
    
    Log: 创建农历日程对重复为每年的闰月添加提醒
    
    Task: https://pms.uniontech.com/task-view-109985.html
    Change-Id: I31c72f1ca819e5abeb2ef983ef5470d5e4cd9174

--- calendar-client/src/dialog/scheduledlg.cpp
+++ calendar-client/src/dialog/scheduledlg.cpp
@@ -22,6 +22,7 @@
 #include "timeedit.h"
 #include "constants.h"
 #include "cscheduleoperation.h"
+#include "cdynamicicon.h"
 
 #include <DHiDPIHelper>
 #include <DFontSizeManager>
@@ -256,10 +257,20 @@ bool CScheduleDlg::clickOkBtn()
             //每月
             ruleID = RepetitionRule::RRule_EVEMONTH;
             break;
-        case 2:
+        case 2: {
             //每年
             ruleID = RepetitionRule::RRule_EVEYEAR;
-            break;
+            //如果该日程为闰月日程，因为对应的闰月需要间隔好多年，所以添加对应的提示信息
+            CaHuangLiDayInfo huangLiInfo;
+            CScheduleDBus::getInstance()->GetHuangLiDay(beginDateTime.date(), huangLiInfo);
+            if (huangLiInfo.mLunarMonthName.contains("闰")) {
+                DCalendarDDialog *prompt = new DCalendarDDialog(this);
+                prompt->setIcon(QIcon(CDynamicIcon::getInstance()->getPixmap()));
+                prompt->setMessage(tr("You have selected a leap month, and will be reminded according to the rules of the lunar calendar."));
+                prompt->addButton(tr("OK", "button"), true, DDialog::ButtonNormal);
+                prompt->exec();
+            }
+        } break;
         default:
             //默认不重复
             ruleID = RepetitionRule::RRule_NONE;
