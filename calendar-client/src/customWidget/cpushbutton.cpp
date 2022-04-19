64a2e50b53aecb14ff500d9474e13d19a36a7ed8
leilong <leilong@uniontech.com>
2022-04-19 16:20:24 +0800

    fix: 修复日程类型下拉框的选中效果问题和1.0之外的缩放比时图片出现齿距问题
     调整日程类型下拉框的焦点传递策略和控制类型下拉框与添加按钮之间的选中效果，调整绘制的图片的初始大小
    
    Log: 修复日程类型下拉框的选中效果问题和1.0之外的缩放比时图片出现齿距问题
    
    Bug: https://pms.uniontech.com/bug-view-125853.html
    Bug: https://pms.uniontech.com/bug-view-125753.html
    Change-Id: I7ea57e9813825f350fe50acfde44f65e463bd6a2

--- calendar-client/src/customWidget/cpushbutton.cpp
+++ calendar-client/src/customWidget/cpushbutton.cpp
@@ -1,57 +1,86 @@
+/*
+* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     leilong  <leilong@uniontech.com>
+*
+* Maintainer: leilong  <leilong@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "cpushbutton.h"
 #include <DPaletteHelper>
 #include <DHiDPIHelper>
-#include <DIconButton>
+#include <DGuiApplicationHelper>
 #include <QMouseEvent>
 #include <QPainter>
-#include <QLabel>
 #include <QHBoxLayout>
 #include <QDebug>
 
-DWIDGET_USE_NAMESPACE
-
-CPushButton::CPushButton(QWidget *parent) : QPushButton(parent)
+CPushButton::CPushButton(QWidget *parent) : QWidget(parent)
 {
-    setFlat(true);
-
-
     QHBoxLayout *layoutAddType = new QHBoxLayout();
-    QLabel * lblAddType = new QLabel();
-    lblAddType->setFixedSize(100,34);
-    lblAddType->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
-    lblAddType->setText(tr("New event type"));
+    m_textLabel = new QLabel(tr("New event type"));
 
-    lblAddType->setFixedSize(200,34);
+    m_textLabel->setFixedSize(100,34);
+    m_textLabel->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+
+    m_textLabel->setFixedSize(200,34);
     layoutAddType->setSpacing(0);
     layoutAddType->setMargin(0);
     layoutAddType->setAlignment(Qt::AlignLeft);
-    DIconButton*  iconBtnAddType = new DIconButton(this);
-    iconBtnAddType->setFixedSize(16, 16);
-    iconBtnAddType->setFlat(true);
-    iconBtnAddType->setIcon(QIcon(DHiDPIHelper::loadNxPixmap(":/resources/icon/create.svg")));
+    m_iconButton = new DIconButton(this);
+    m_iconButton->setFocusPolicy(Qt::NoFocus);
+    m_iconButton->setFixedSize(16, 16);
+    m_iconButton->setFlat(true);
+
+    DPalette palette = DPaletteHelper::instance()->palette(this);
+    QPalette pa = m_textLabel->palette();
+
+    //设置深浅色主题下正常状态时的文本颜色，与下拉框颜色对其
+    if (DGuiApplicationHelper::instance()->themeType() == DGuiApplicationHelper::DarkType) {
+        pa.setBrush(QPalette::WindowText, QColor("#FFFFFF"));
+    } else {
+        pa.setBrush(QPalette::WindowText, QColor("#000000"));
+    }
+    m_textLabel->setPalette(pa);
+
     layoutAddType->setContentsMargins(33,0,0,0);
-    layoutAddType->addWidget(iconBtnAddType);
+    layoutAddType->addWidget(m_iconButton);
     layoutAddType->addSpacing(5);
-    layoutAddType->addWidget(lblAddType);
+    layoutAddType->addWidget(m_textLabel);
     setFixedHeight(34);
     setLayout(layoutAddType);
+}
+
+void CPushButton::setHighlight(bool status)
+{
+    if (status == m_Highlighted) {
+        return;
+    }
+    m_Highlighted = status;
+    update();
+}
+
+bool CPushButton::isHighlight()
+{
+    return m_Highlighted;
 }
 
 void CPushButton::mousePressEvent(QMouseEvent *event)
 {
     Q_UNUSED(event);
     m_pressed = true;
-    update();
-}
-
-void CPushButton::mouseMoveEvent(QMouseEvent *event)
-{
-    if (rect().contains(event->pos())) {
-        m_pressed = true;
-    } else {
-        m_pressed = false;
-    }
-    update();
 }
 
 void CPushButton::mouseReleaseEvent(QMouseEvent *event)
@@ -60,25 +89,34 @@ void CPushButton::mouseReleaseEvent(QMouseEvent *event)
         emit clicked();
     }
     m_pressed = false;
-    update();
 }
 
 void CPushButton::paintEvent(QPaintEvent *event)
 {
-    QPushButton::paintEvent(event);
+    QWidget::paintEvent(event);
     DPalette palette = DPaletteHelper::instance()->palette(this);
     QPainter painter(this);
     // 反走样
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setPen(Qt::NoPen);
 
-    if (m_pressed) {
+    if (m_Highlighted) {
         //背景设置为高亮色
         painter.setBrush(palette.highlight());
+        m_textLabel->setBackgroundRole(QPalette::Highlight);
+        //高亮状态显示白色图片
+        m_iconButton->setIcon(QIcon(DHiDPIHelper::loadNxPixmap(":/resources/icon/create_white.svg")));
     } else {
         //背景透明
         painter.setBrush(QBrush("#00000000"));
+        m_textLabel->setBackgroundRole(QPalette::Window);
+        //深色主题下只能是白色图片，浅色主题下显示黑色图片
+        if (DGuiApplicationHelper::instance()->themeType() == DGuiApplicationHelper::DarkType) {
+            m_iconButton->setIcon(QIcon(DHiDPIHelper::loadNxPixmap(":/resources/icon/create_white.svg")));
+        } else {
+            m_iconButton->setIcon(QIcon(DHiDPIHelper::loadNxPixmap(":/resources/icon/create_black.svg")));
+        }
     }
-
+    //绘制背景
     painter.drawRect(this->rect());
 }
--- calendar-client/src/customWidget/jobtypecombobox.h
+++ calendar-client/src/customWidget/jobtypecombobox.h
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     leilong  <leilong@uniontech.com>
+*
+* Maintainer: leilong  <leilong@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef JOBTYPECOMBOBOX_H
 #define JOBTYPECOMBOBOX_H
 
@@ -25,16 +45,23 @@ public slots:
 
 protected slots:
     void slotBtnAddItemClicked();
+
+protected:
     void showPopup() override;
+    bool eventFilter(QObject*, QEvent*) override;
 
 private:
     void initUI();
     void addJobTypeItem(int idx, QString strColorHex, QString strJobType);
     void addCustomWidget(QFrame *);
+    void setItemSelectable(bool status);
 
 private:
     QWidget *m_customWidget {nullptr};
+    CPushButton *m_addBtn {nullptr};
     QList<JobTypeInfo> m_lstJobType;
+    int m_hoverSelectedIndex = -1; //鼠标悬停的选项下标
+    int m_itemNumIndex = 0; //item数量
 };
 
 #endif // JOBTYPECOMBOBOX_H
